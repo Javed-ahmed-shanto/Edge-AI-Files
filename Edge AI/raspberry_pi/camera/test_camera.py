4c077fad4a4c3a5e5b7b6932875fa4879ba2d789
shanto <shantoa729@gmail.com>
2024-04-24 20:07:17 +0900

    Add files via upload

---
+++ Edge AI/raspberry_pi/camera/test_camera.py
@@ -0,0 +1,24 @@
+import cv2
+
+def test_camera(camera_id):
+    cap = cv2.VideoCapture(camera_id)
+
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+    else:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Couldn't read frames.")
+                break
+
+            cv2.imshow("Camera Test", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    test_camera(1)  # Change the camera index if needed
